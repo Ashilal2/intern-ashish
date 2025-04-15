bc394038c843094578de46da6b55b09d1232a004
ashilal1 <ashishlalshrestha10ban@gmail.com>
2025-04-15 19:34:46 +0900

    serviceを使って操作ログを記録・表示できるようにしました。

--- intern-book-app/src/app/app.component.ts
+++ intern-book-app/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { CardComponent } from "./card/card.component";
 import { BookFormComponent } from "./book-form/book-form.component";
+import { MessageComponent } from './message/message.component';
 
 
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [ToolbarComponent, BookFormComponent],
+  imports: [ToolbarComponent, BookFormComponent, MessageComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
